db05903d30578f3e7a90da4fac2c7afa57a16dc4
Jahidul Haque Shuvo <jahidulhaque1921@gmail.com>
2025-04-28 15:11:36 +0600

    pages

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function AboutPage() {
+    return (
+      <div className="p-4">
+        <h1 className="text-2xl font-bold">About Us</h1>
+        <p className="mt-2">
+          This is the About page. Here you can add information about your website or company.
+        </p>
+      </div>
+    );
+  }
+  
---
+++ app/services/page.tsx
@@ -0,0 +1,21 @@
+export default function ServicesPage() {
+    return (
+      <div className="p-4">
+        <h1 className="text-2xl font-bold">Our Services</h1>
+        <p className="mt-2">
+          At MySite, we offer a variety of services to meet your needs. Our team of experts is here to help you with:
+        </p>
+        <ul className="list-disc mt-4 ml-5">
+          <li>Web Development</li>
+          <li>Mobile App Development</li>
+          <li>Digital Marketing</li>
+          <li>Graphic Design</li>
+          <li>Consulting and Strategy</li>
+        </ul>
+        <p className="mt-4">
+          Contact us to learn more about how we can help you grow your business and achieve your goals.
+        </p>
+      </div>
+    );
+  }
+  
