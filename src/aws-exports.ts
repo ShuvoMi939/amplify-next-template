c4764b5a73a211bc2c375843f588addb01a3cbd2
Jahidul Haque Shuvo <jahidulhaque1921@gmail.com>
2025-04-29 01:16:00 +0600

    okkk

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -1,9 +1,8 @@
 const awsmobile = {
-    aws_project_region: 'your-region',
-    aws_cognito_identity_pool_id: 'your-identity-pool-id',
-    aws_cognito_region: 'your-region',
-    aws_user_pools_id: 'your-user-pool-id',
-    aws_user_pools_web_client_id: 'your-app-client-id',
+    aws_project_region: "ap-south-1", // your region
+    aws_cognito_region: "ap-south-1", // same region
+    aws_user_pools_id: "ap-south-1_SyXrMqcX3", // user pool id
+    aws_user_pools_web_client_id: "s8g84kbc4uuks08fqj2pqftdj", // app client id
     oauth: {},
   };
   
