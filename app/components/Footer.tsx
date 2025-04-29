051bad1a1a91754ad47e69ffdabae2f2a35880b6
Jahidul Haque Shuvo <jahidulhaque1921@gmail.com>
2025-04-29 18:08:50 +0600

    mt-12 remove

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
     return (
-      <footer className="bg-gray-800 text-white py-4 mt-12">
+      <footer className="bg-gray-800 text-white py-4">
         <div className="max-w-screen-xl mx-auto text-center">
           <p>&copy; {new Date().getFullYear()}, Nirdeshona. All rights reserved.</p>
         </div>
